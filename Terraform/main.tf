bf271c41cd2f378ec3fa72d56768a7354ae461ef
Nicolay Chepurnenko <nchepurnenko@mail.ru>
2021-08-04 08:55:52 +0000

    07-terraform-03-basic

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,6 +2,24 @@ provider "aws" {
   region = "us-west-2"
 }
 
+locals {
+  test_workspace_map = {
+    stage = "t2.nano"
+    prod = "t2.micro"
+  }
+
+  test_workspace_instance_count = {
+    stage = 1
+    prod = 2
+  }
+
+  test_workspace_instance_count_fe = {
+    "1" = 1
+    "2" = 2
+    "3" = 3
+  }
+}
+
 data "aws_ami" "ubuntu" {
     most_recent = true
 
@@ -34,7 +52,9 @@ resource "aws_network_interface" "test_interface" {
 
 resource "aws_instance" "test" {
     ami           = "${data.aws_ami.ubuntu.id}"
-    instance_type = "t2.micro"
+    # instance_type = "t2.micro"
+    instance_type = local.test_workspace_map[terraform.workspace]
+    count = local.test_workspace_instance_count[terraform.workspace]
     availability_zone = "us-west-2"
     key_name = "test_key_pair_1"
 
@@ -46,6 +66,16 @@ resource "aws_instance" "test" {
     tags = {
         Name = "Test ec2 instance"
     }
+    lifecycle {
+      create_before_destroy = true
+    }
+}
+
+resource "aws_instance" "test2" {
+    for_each = toset(["1","2"])
+    ami           = "${data.aws_ami.ubuntu.id}"
+    instance_type = local.test_workspace_map[terraform.workspace]
+    availability_zone = "us-west-2"
 }
 
 data "aws_caller_identity" "identity" {}
--- Terraform/versions.tf
+++ Terraform/versions.tf
@@ -5,4 +5,10 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "s3" {
+    bucket = "testnetologytfbackend"
+    key    = "terraform.tfstate"
+    region = "us-west-2"
+  }
 }
